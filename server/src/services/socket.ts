f1d68db1fa3364bf3a0420f4affe3a139348d986
--list <56119252+SujeetYT@users.noreply.github.com>
2024-10-22 20:36:53 +0530

    fixed redis issue on production

--- server/src/services/socket.ts
+++ server/src/services/socket.ts
@@ -1,14 +1,12 @@
 import { Server } from "socket.io";
 import Redis from "ioredis";
 
-const redisCredentials = {
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,  
+const redisUrl = process.env.REDIS_URL;
+if (!redisUrl) {
+  throw new Error("REDIS_URL environment variable is not defined");
 }
-const pub = new Redis(redisCredentials)
-const sub = new Redis(redisCredentials)
+const pub = new Redis(redisUrl, { tls: { rejectUnauthorized: false }})
+const sub = new Redis(redisUrl, { tls: { rejectUnauthorized: false }})
 
 
 
@@ -45,7 +43,7 @@ class SocketService {
 
       // collect all socket id and push it to redis
       pub.sadd("SOCKETS", socket.id)
-      
+
       // get all active sockets if new connection added
       const allActiveSockets = await pub.smembers("SOCKETS")
       io.emit("event:sockets", allActiveSockets);
